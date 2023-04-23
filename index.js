9ae66c0ecd31ed2c00f5d540de3290cb04dc7e52
Your Name <you@example.com>
2023-04-24 01:49:44 +0200

    http methods to get and post users

--- index.js
+++ index.js
@@ -2,24 +2,45 @@ const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 // Request Body parsing Middleware
 
-const dbConnection= require('./src/utils/mysql.connector')
+const dbConnection = require('./src/utils/mysql.connector')
+const { query } = require('express')
 
-const post = require('./src/posts/posts.model')
+const {User} = require('./src/users/users.model')
 
-app.get('/api/v1', function(req, res) {
-    return res.json(req.headers)
+
+//get users to the database
+app.get('/api/users', function (req, res) {
+    var sql = "SELECT * FROM users"
+    return dbConnection.query(sql, function (err, result) {
+        if (err) throw err;
+
+        return res.json(result)
+    })
 })
 
-app.get('/api/v1/posts', function(req, res) {
-    return res.json([posts])
+//post users to the database
+app.post('/api/users', function (req, res) {
+    
+    const {userid, username, email, phone_number, address, age, gender} = req.body
+    var sql = `INSERT INTO users(userid, username, email, phone_number, address, age, gender) VALUES ('${userid}','${username}','${email}','${phone_number}','${address}','${age}','${gender}')`;
+        
+    return dbConnection.query(sql, function (err, result) { 
+        if (err) throw err;
+        
+        return res.json(result)
+     });
 })
 
-app.listen(5000, function(){
+ app.listen(5000, function(){
     console.log('TravelLand is listening on port 3000')
 
     dbConnection.connect( function (err){
+        if (err) throw err
         
         console.log("Connected to MySQL")
     })
