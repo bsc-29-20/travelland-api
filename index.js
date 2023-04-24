b67a178cb7f6838605c86d00525ddd9b0a3c1e00
Your Name <you@example.com>
2023-04-24 11:11:16 +0200

    Update user method

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Request Body parsing Middleware
 
 const dbConnection = require('./src/utils/mysql.connector')
-const { query } = require('express')
+const {query} = require('express')
 
 const {User} = require('./src/users/users.model')
 
@@ -35,6 +35,42 @@ app.post('/api/users', function (req, res) {
         return res.json(result)
      });
 })
+
+//Update users using patch
+app.patch('/api/users/:userid', function (req, res) {
+    
+    const sql = `SELECT * FROM posts WHERE userid=${req.params.userid} LIMIT 1`
+
+    return dbConnection.query(sql, function (err, rows) {
+        if (err) throw err
+
+        const user = request.body
+        
+        if (rows.length >= 1) {
+            let props = ''
+
+            props = Object.keys(user).map((key, index) => {
+                               
+                return props += `${[key]}='${Object.values(user).at(index)}',`
+            })
+            
+            const updateSql = `UPDATE posts SET ${props[props.length - 1].slice(0, -1)} WHERE userid=${rows[0].userid}`
+            
+            return dbConnection.query(updateSql, function (err, result) {
+                return res.json(rows[0])
+            })
+        } else {
+            return res.status(404).json({
+                status: false,
+                statusCode: 404,
+                message: `User with userid ${request.params.userid} does not exist.`
+            })
+        }
+    })
+})
+
+
+
 
  app.listen(5000, function(){
     console.log('TravelLand is listening on port 3000')
